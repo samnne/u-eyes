969fec810c436d09c3ae42f92cdbc3ff37cb84c2
Samuel Echefu <samuelnnechefu@gmail.com>
2026-02-09 17:18:24 -0800

    stupid cors

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://samnne.github.io/u-eyes/",
+        "https://samnne.github.io/",
     ],  # http://localhost:5173
     # allow_credentials=False,
     # allow_methods=["*"],
